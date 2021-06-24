9640e9903090b539ba168ddd20831e2fa59c5909
Raquel Ferreira <44173946+raquel-f@users.noreply.github.com>
2021-06-24 01:19:15 +0100

    Added CORS enabling

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 // import routers
 const jobBoardRouter = require("./routes/jobPost");
@@ -18,6 +18,14 @@ app.use(
 );
 
 app.get("/", (req, res) => {
+  // cors enabling
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+
+  // api is ready
   res.json({ message: "ready!" });
 });
 
