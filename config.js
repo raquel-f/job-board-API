27361290a8ac5a37ede2ed03b4f7ab16c396ba2b
Raquel Ferreira <44173946+raquel-f@users.noreply.github.com>
2021-06-24 18:12:09 +0100

    added proper config file

---
+++ config.js
@@ -0,0 +1,17 @@
+// configurations: the database credentials and rows per page we want to show when we paginate results
+require('dotenv').config();
+
+const env = process.env;
+
+const config = {
+    /* sensitive info kept in .env file (dev) and environment variables (prod) */
+    db: { 
+        host: env.DB_HOST,
+        user: env.DB_USER,
+        password: env.DB_PASSWORD,
+        database: env.DB_NAME,
+    },
+    listPerPage: env.LIST_PER_PAGE || 10,
+};
+
+module.exports = config;
