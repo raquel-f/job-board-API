3df812906bde1e6b5da74438f3eb263cd06292f6
Raquel Ferreira <44173946+raquel-f@users.noreply.github.com>
2021-07-17 19:22:06 +0100

    database configuration changes due to db hosting modifications

--- config.js
+++ config.js
@@ -1,19 +1,23 @@
 // configurations: the database credentials and rows per page we want to show when we paginate results
-require('dotenv').config();
+require("dotenv").config();
+const parseURL = require("parse-database-url");
 
 const env = process.env;
 
+// parse database information from database URL
+const dbConfig = parseURL(env.CLEARDB_DATABASE_URL);
+
 const config = {
-    /* sensitive info kept in .env file (dev) and environment variables (prod) */
-    db: { 
-        host: env.DB_HOST,
-        user: env.DB_USER,
-        password: env.DB_PASSWORD,
-        database: env.DB_NAME,
-    },
-    listPerPage: env.LIST_PER_PAGE || 10,
-    authAudience: env.AUTH0_AUDIENCE, 
-    authDomain: env.AUTH0_DOMAIN,
+  /* sensitive info kept in .env file (dev) and environment variables (prod) */
+  db: {
+    host: dbConfig.host,
+    user: dbConfig.user,
+    password: dbConfig.password,
+    database: dbConfig.database,
+  },
+  listPerPage: env.LIST_PER_PAGE || 10,
+  authAudience: env.AUTH0_AUDIENCE,
+  authDomain: env.AUTH0_DOMAIN,
 };
 
 module.exports = config;
